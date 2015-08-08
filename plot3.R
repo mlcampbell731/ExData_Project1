85c8ac1b78d56c08bca70d4bba32bebe3525abd1
mlcampbell731 <mlcampbell731@gmail.com>
2015-08-08 18:11:44 -0400

    Changes to stop legend getting cut off

--- plot3.R
+++ plot3.R
@@ -12,12 +12,12 @@ datetime <- strptime(paste(hpc_sub$Date, hpc_sub$Time), "%d/%m/%Y %H:%M:%S")
 
 
 par(mar = c(5,5,4,2))
+png("plot3.png", width = 480, height = 480)
 
 plot(datetime, hpc_sub$Sub_metering_1, type="s", xlab = "", ylab = "Energy sub metering")
 lines(datetime, hpc_sub$Sub_metering_2, col = "red")
 lines(datetime, hpc_sub$Sub_metering_3, col = "blue")
 
-legend()
+legend("topright", lty = "solid", col = c("black", "red", "blue"), legend = names(hpc_sub)[7:9], cex = 1)
 
-dev.copy(png, file = "plot3.png")
 dev.off()
