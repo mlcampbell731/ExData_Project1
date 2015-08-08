240f46c55f29cf65b51d3a4b4f0df2cf8bca2f1e
mlcampbell731 <mlcampbell731@gmail.com>
2015-08-08 17:52:28 -0400

    Fixed code, lines look correct, still raw not refined

--- plot3.R
+++ plot3.R
@@ -7,12 +7,17 @@ library(dplyr)
 hpc <- read_csv2("household_power_consumption.txt", col_names=TRUE, col_types="ccnnnnnnn")
 
 hpc_sub <- filter(hpc, as.Date(hpc$Date, "%d/%m/%Y") == "2007-02-01" | as.Date(hpc$Date, "%d/%m/%Y") == "2007-02-02")
-hpc_sub <- mutate(hpc_sub, Day = format(strptime(paste(hpc_sub$Date, hpc_sub$Time), "%d/%m/%Y %H:%M:%S"), '%a'))
+
+datetime <- strptime(paste(hpc_sub$Date, hpc_sub$Time), "%d/%m/%Y %H:%M:%S")
 
 
 par(mar = c(5,5,4,2))
 
-plot(hpc_sub$Sub_metering_3,ylab = "Energy Sub Metering", ann = FALSE)
-lines(hpc_sub$Sub_metering_1, col = "black")
-lines(hpc_sub$Sub_metering_2, col = "red")
-lines(hpc_sub$Sub_metering_3, col = "blue")
+plot(datetime, hpc_sub$Sub_metering_1, type="s", xlab = "", ylab = "Energy sub metering")
+lines(datetime, hpc_sub$Sub_metering_2, col = "red")
+lines(datetime, hpc_sub$Sub_metering_3, col = "blue")
+
+legend()
+
+dev.copy(png, file = "plot3.png")
+dev.off()
