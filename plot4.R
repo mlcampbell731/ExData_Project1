78ef824240cf6e5143c85606c040d2bb52d67bc3
mlcampbell731 <mlcampbell731@gmail.com>
2015-08-08 18:28:44 -0400

    First run to see if code works/how it looks

---
+++ plot4.R
@@ -0,0 +1,34 @@
+install.packages("readr")
+library(readr)
+
+install.packages("dplyr")
+library(dplyr)
+
+hpc <- read_csv2("household_power_consumption.txt", col_names=TRUE, col_types="ccnnnnnnn")
+
+hpc_sub <- filter(hpc, as.Date(hpc$Date, "%d/%m/%Y") == "2007-02-01" | as.Date(hpc$Date, "%d/%m/%Y") == "2007-02-02")
+
+datetime <- strptime(paste(hpc_sub$Date, hpc_sub$Time), "%d/%m/%Y %H:%M:%S")
+
+
+par(mfrow = c(2,2))
+png("plot4.png", width = 480, height = 480)
+
+#plot upper left
+plot(datetime, hpc_sub$Global_active_power, type="l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+#plot upper right
+plot(datetime, hpc_sub$Voltage, type="l", ylab = "Voltage")
+
+#plot lower left
+plot(datetime, hpc_sub$Sub_metering_1, type="s", xlab = "", ylab = "Energy sub metering")
+lines(datetime, hpc_sub$Sub_metering_2, col = "red")
+lines(datetime, hpc_sub$Sub_metering_3, col = "blue")
+
+legend("topright", lty = "solid", col = c("black", "red", "blue"), bty = "n", legend = names(hpc_sub)[7:9], cex = 0.5)
+
+#plot lower right
+plot(datetime, hpc_sub$Global_reactive_power, ylab = "Global_reactive_power",type="l")
+
+
+dev.off()
