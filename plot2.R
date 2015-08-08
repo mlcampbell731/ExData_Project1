c1e8f5513d9e7028981e2177ff0b40d5c9c75a96
mlcampbell731 <mlcampbell731@gmail.com>
2015-08-08 17:45:42 -0400

    Final tweaks to get everything correct (labels, axes, graph)

--- plot2.R
+++ plot2.R
@@ -7,11 +7,12 @@ library(dplyr)
 hpc <- read_csv2("household_power_consumption.txt", col_names=TRUE, col_types="ccnnnnnnn")
 
 hpc_sub <- filter(hpc, as.Date(hpc$Date, "%d/%m/%Y") == "2007-02-01" | as.Date(hpc$Date, "%d/%m/%Y") == "2007-02-02")
-time_change <- strptime(paste(hpc_sub$Date, hpc_sub$Time), "%d/%m/%Y %H:%M:%S")
+
+datetime <- strptime(paste(hpc_sub$Date, hpc_sub$Time), "%d/%m/%Y %H:%M:%S")
 
 
 par(mar = c(5,5,4,2))
-plot(hpc_sub$Global_active_power, type="l", ylab = "Global Active Power (kilowatts)")
+plot(datetime, hpc_sub$Global_active_power, type="l", xlab = "", ylab = "Global Active Power (kilowatts)")
 
 
 
