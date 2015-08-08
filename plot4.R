54f3906a4b02abe756cd87e1304983deb50e83b3
mlcampbell731 <mlcampbell731@gmail.com>
2015-08-08 18:32:31 -0400

    Fixed legend and label problems

--- plot4.R
+++ plot4.R
@@ -11,8 +11,8 @@ hpc_sub <- filter(hpc, as.Date(hpc$Date, "%d/%m/%Y") == "2007-02-01" | as.Date(h
 datetime <- strptime(paste(hpc_sub$Date, hpc_sub$Time), "%d/%m/%Y %H:%M:%S")
 
 
-par(mfrow = c(2,2))
 png("plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2))
 
 #plot upper left
 plot(datetime, hpc_sub$Global_active_power, type="l", xlab = "", ylab = "Global Active Power (kilowatts)")
@@ -25,10 +25,9 @@ plot(datetime, hpc_sub$Sub_metering_1, type="s", xlab = "", ylab = "Energy sub m
 lines(datetime, hpc_sub$Sub_metering_2, col = "red")
 lines(datetime, hpc_sub$Sub_metering_3, col = "blue")
 
-legend("topright", lty = "solid", col = c("black", "red", "blue"), bty = "n", legend = names(hpc_sub)[7:9], cex = 0.5)
+legend("topright", lty = "solid", col = c("black", "red", "blue"), bty = "n", legend = names(hpc_sub)[7:9])
 
 #plot lower right
 plot(datetime, hpc_sub$Global_reactive_power, ylab = "Global_reactive_power",type="l")
-
 
 dev.off()
