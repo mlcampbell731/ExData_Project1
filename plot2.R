1d4495f90250e0cad53bf8bf1db9b104f9ae7c83
mlcampbell731 <mlcampbell731@gmail.com>
2015-08-07 22:01:23 -0400

    First draft, raw output, no tweaks to aesthetics

---
+++ plot2.R
@@ -0,0 +1,16 @@
+install.packages("readr")
+library(readr)
+
+install.packages("dplyr")
+library(dplyr)
+
+hpc <- read_csv2("household_power_consumption.txt", col_names=TRUE, col_types="ccnnnnnnn")
+
+hpc_sub <- filter(hpc, as.Date(hpc$Date, "%d/%m/%Y") == "2007-02-01" | as.Date(hpc$Date, "%d/%m/%Y") == "2007-02-02")
+
+par(mar = c(5,5,4,2))
+plot(hpc_sub$Global_active_power, type="l", ylab = "Global Active Power (kilowatts)")
+
+
+dev.copy(png, file = "plot1.png")
+dev.off()
